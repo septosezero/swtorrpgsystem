519554607934b6efc111be93c9be62808947a671
septosezero <35184029+septosezero@users.noreply.github.com>
2018-03-02 02:12:24 -0500

    Update talenttree.lua

--- talenttree.lua
+++ talenttree.lua
@@ -27,93 +27,107 @@ DarkRPG.createJobSkills({
 
 	-- player stats
 	stats = {
-		health = '1',
-		movement = '-5%',
-		jump = '-10%',
+		health = '50',
+		movement = '-25%',
+		jump = '-25%',
 
-		armor = '20',
-		evasion = '2%',
-		reflect = '2%',
+		armor = '50',
+		evasion = '10%',
+		reflect = '10%',
 
-		salary = '2%',
-		merchant = '0%',
-		prison = '1%',
+		salary = '10%',
+		merchant = '-50%',
+		prison = '-50%',
 
-		-- damage resists
-		resists = '1%', -- all incoming sources of damage 
-		burn = '1%', -- burn, ignite, shock, dissolve
-		endurance = '1%', -- poison, nerve, drowning, radiation, acid
-		crush = '1%', -- crush, vehicle crush, falling damage, physgun
-		explode = '1%', -- explosives, plasma, sonicboom
-
-		-- applies to all weapons
+			-- applies to all weapons
 		damage = '1%',
 		critical = '1%',
 		firerate = '1%',
 		magazine = '1%',
 		ammo = '1%',
-	},
-
-	-- applies to a specific set of weapons
-	weaponcategory = {
-		name = "Handguns", -- must match name you set in 'weapons.lua' EXACTLY
-		stats = {
-			damage = '1%',
-			critical = '1%',
-			firerate = '1%',
-			magazine = '1%',
-			ammo = '1%',
-		},
 	},
 })
 
 -- adds a talent button
 DarkRPG.createTalent({
-	name = "My_Example_Talent",
-	team = {TEAM_EXAMPLE1, TEAM_EXAMPLE2},
+	name = "Blaster Rifle Training",
+	team = RepublicArmy, ImpArmy
 	pos = {1,1}, -- x and y on the talent tree
-	thumb = "myexample.png", -- must be in '/materials/darkrpg2/myexample.png'
+	thumb = "BlasterRifles.png", -- must be in '/materials/darkrpg2/myexample.png'
 	ranks = 9, -- ranks 1 through 9
-	desc = "This is my example talent, delete the 'desc' line if you want the tooltip text generated automatically! ",
-
-	-- player stats
-	stats = {
-		health = '1',
-		movement = '1%',
-		jump = '1%',
-
-		armor = '1',
-		evasion = '1%',
-		reflect = '1%',
-
-		salary = '1%',
-		merchant = '1%',
-		prison = '1%',
-
-		-- damage resists
-		resists = '1%', -- all incoming sources of damage 
-		burn = '1%', -- burn, ignite, shock, dissolve
-		endurance = '1%', -- poison, nerve, drowning, radiation, acid
-		crush = '1%', -- crush, vehicle crush, falling damage, physgun
-		explode = '1%', -- explosives, plasma, sonicboom
-
-		-- applies to all weapons
-		damage = '1%',
-		critical = '1%',
-		firerate = '1%',
-		magazine = '1%',
-		ammo = '1%',
-	},
-
-	-- applies to a specific set of weapons
-	weaponcategory = {
-		name = "MK9 Shotguns", -- must match name you set in 'weapons.lua' EXACTLY
-		stats = {
-			damage = '1%',
-			critical = '1%',
-			firerate = '1%',
-			magazine = '1%',
-			ammo = '1%',
-		},
-	},
+	give = "weapon_tfa_kotor_br_1",
+	desc = "Proficiency in Blaster Rifles allows you to carry one at all times, and increases your ability with them.",
 })
+
+DarkRPG.createTalent({
+	name = "Blaster Pistol Proficiency",
+	team = RepublicArmy, ImpArmy
+	pos = {1,2},
+	thumb = "BlasterPistols.png",
+	ranks = 9,
+	give = "weapon_tfa_kotor_bp_2",
+	desc = "Proficiency in Blaster Pistols allows you to carry one at all times, and increases your ability with them.",
+})
+
+DarkRPG.createTalent({
+	name = "Heavy Weapons Proficiency",
+	team = RepublicArmy, ImpArmy
+	pos = {1,3},
+	thumb = "HeavyWeapons.png",
+	ranks = 9,
+	give = "weapon_tfa_swch_z6_green",
+	desc = "Proficiency in Heavy Weapons allows you to carry one at all times, and increases your ability with them.",
+})
+
+DarkRPG.createTalent({
+	name = "Medic Training",
+	team = RepublicArmy, ImpArmy
+	pos = {2,1},
+	thumb = "MedicTraining.png",
+	ranks = 9,
+	give = {"weapon_defibrillator", "weapon_bactainjector"}
+	desc = "Medic Training allows you to revive and heal allies.",
+})
+
+DarkRPG.createTalent({
+	name = "Sniper Training",
+	pos = {2,2},
+	thumb = "SniperTraining.png",
+	ranks = 9,
+	give = "tfa_wsp_3",
+	desc = "Sniper training allows you to carry a sniper at all times, and increases your ability with them.",
+})
+
+DarkRPG.createTalent({
+	name = "Engineer Training",
+	pos = {2,3},
+	thumb = "EngineerTraining.png",
+	ranks = 9,
+	give = "tfa_swch_clonelauncher",
+	desc = "Engineer training allows you to carry a launcher at all times, and increases your ability with them.",
+})
+
+DarkRPG.createTalent({
+	name = "Steady Aim",
+	post = {3,2},
+	thumb = "SteadyAim.png",
+	ranks = 9,
+	desc = "Steady Aim increases your proficiency with all weapons."
+})
+
+DarkRPG.createTalent({
+	name = "Power Shot",
+	pos = {3,3},
+	thumb = "PowerShot.png",
+	ranks = 9,
+	desc "Power Shot increases your damage with all weapons."
+})
+
+DarkRPG.createTalent({
+	name = "Conditioning",
+	pos = {4,3},
+	thumb = "Conditioning.png",
+	ranks = 9,
+	desc "Conditioning increases your jump and movement speed."
+})
+
