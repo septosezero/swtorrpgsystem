6aeab6f995238e8f8cc1e8f41faccc7832582ec2
septosezero <35184029+septosezero@users.noreply.github.com>
2018-03-02 00:04:55 -0800

    Add files via upload

---
+++ weapons.lua
@@ -0,0 +1,231 @@
+DarkRPG.createWeaponCategory({
+	name = 'Rifles',
+	weapons = {
+		'weapon_ak472',
+		'weapon_m42',
+		'weapon_ar2',
+		'm9k_tar21',
+		'm9k_auga3',
+		'm9k_vikhr',
+		'm9k_scar',
+		'm9k_m4a1',
+		'm9k_m16a4_acog',
+		'm9k_m14sp',
+		'm9k_l85',
+		'm9k_g3a3',
+		'm9k_m416',
+		'm9k_g36',
+		'm9k_fal',
+		'm9k_famas',
+		'm9k_winchester73',
+		'm9k_acr',
+		'm9k_ak47',
+		'm9k_ak74',
+		'm9k_amd65',
+		'm9k_an94',
+		'm9k_val',
+		'm9k_f2000',
+		'fas2_ak47',
+		'fas2_ak12',
+		'fas2_74',
+		'fas2_an94',
+		'fas2_g3',
+		'fas2_famas',
+		'fas2_g36e',
+		'fas2_galil',
+		'fas2_m4a1',
+		'fas2_m14',
+		'fas2_sg550',
+		'fas2_sg552',
+	}
+})
+
+DarkRPG.createWeaponCategory({
+	name = 'Handguns',
+	weapons = {
+		'weapon_deagle2',
+		'weapon_fiveseven2',
+		'weapon_glock2',
+		'weapon_p2282',
+		'weapon_pistol',
+		'weapon_357',
+		'm9k_colt1911',
+		'm9k_deagle',
+		'm9k_glock',
+		'm9k_usp',
+		'm9k_hk45',
+		'm9k_m92beretta',
+		'm9k_luger',
+		'm9k_sig_p229r',
+		'm9k_coltpython',
+		'm9k_ragingbull',
+		'm9k_scoped_taurus',
+		'm9k_remington1858',
+		'm9k_model3russian',
+		'm9k_model500',
+		'm9k_m29satan',
+		'm9k_model627',
+		'fas2_deagle',
+		'fas2_glock20',
+		'fas2_m1911',
+		'fas2_ots33',
+		'fas2_p226',
+		'fas2_ragingbull',
+	}
+})
+
+DarkRPG.createWeaponCategory({
+	name = 'Shotguns',
+	weapons = {
+		'weapon_pumpshotgun2',
+		'weapon_shotgun',
+		'm9k_browningauto5',
+		'm9k_1887winchester',
+		'm9k_spas12',
+		'm9k_remington870',
+		'm9k_mossberg590',
+		'm9k_ithacam37',
+		'm9k_dbarrel',
+		'm9k_browningauto5',
+		'm9k_m3',
+		'm9k_jackhammer',
+		'm9k_usas',
+		'm9k_striker12',
+		'fas2_ks23',
+		'fas2_m3s90',
+		'fas2_rem870',
+		'fas2_toz34',
+	}
+})
+
+DarkRPG.createWeaponCategory({
+	name = 'Submachine guns',
+	weapons = {
+		'weapon_mac102',
+		'weapon_mp52',
+		'm9k_honeybadger',
+		'm9k_bizonp19',
+		'm9k_smgp90',
+		'm9k_mp5',
+		'm9k_mp7',
+		'm9k_ump45',
+		'm9k_usc',
+		'm9k_kac_pdw',
+		'm9k_vector',
+		'm9k_magpulpdr',
+		'm9k_mp40',
+		'm9k_mp5sd',
+		'm9k_mp9',
+		'm9k_sten',
+		'm9k_tec9',
+		'm9k_thompson',
+		'm9k_uzi',
+		'fas2_mp5a5',
+		'fas2_mp5k',
+		'fas2_mp5sd6',
+		'fas2_pp19',
+		'fas2_uzi',
+	}
+})
+
+DarkRPG.createWeaponCategory({
+	name = 'Machine guns',
+	weapons = {
+		'm9k_minigun',
+		'm9k_m1918bar',
+		'm9k_m249lmg',
+		'm9k_m60',
+		'm9k_pkm',
+		'm9k_ares_shrike',
+		'm9k_fg42',
+		'fas2_rpk',
+	}
+})
+
+DarkRPG.createWeaponCategory({
+	name = 'Sniper Rifles',
+	weapons = {
+		'ls_sniper',
+		'm9k_aw50',
+		'm9k_barret_m82',
+		'm9k_m98b',
+		'm9k_intervention',
+		'm9k_m24',
+		'm9k_svu',
+		'm9k_sl8',
+		'm9k_psg1',
+		'm9k_remington7615p',
+		'm9k_dragunov',
+		'm9k_svt40',
+		'm9k_contender',
+		'fas2_m21',
+		'fas2_m24',
+		'fas2_m82',
+		'fas2_sks',
+		'fas2_sr25',
+	}
+})
+
+DarkRPG.createWeaponCategory({
+	name = 'Melee and Non-Lethals',
+	weapons = {
+		'stunstick',
+		'arrest_stick',
+		'unarrest_stick',
+		'weaponchecker',
+		'weapon_crowbar',
+		'weapon_fists',
+		'm9k_harpoon',
+		'm9k_knife',
+		'm9k_machete',
+		'm9k_damascus',
+		'm9k_fists',
+		'fas2_dv2',
+		'fas2_machete',
+	}
+})
+
+DarkRPG.createWeaponCategory({
+	name = 'Poopslinging',
+	weapons = {
+		'weapon_bugbait',
+	}
+})
+
+DarkRPG.createWeaponCategory({
+	name = 'Explosives',
+	weapons = {
+		'weapon_frag',
+		'weapon_slam',
+		'weapon_rpg',
+		'm9k_davy_crockett',
+		'm9k_m61_frag',
+		'm9k_ied_detonator',
+		'm9k_m202',
+		'm9k_m79gl',
+		'm9k_matador',
+		'm9k_milkormgl',
+		'm9k_nitro',
+		'm9k_orbital_strike',
+		'm9k_proxy_mine',
+		'm9k_rpg7',
+		'm9k_sticky_grenade',
+		'm9k_suicide_bomb',
+		"fas2_m67",
+		"fas2_m79",
+	}
+})
+
+DarkRPG.createWeaponCategory({
+	name = 'Medical Equipment',
+	weapons = {
+		'med_kit',
+	}
+})
+
+DarkRPG.createWeaponCategory({
+	name = 'Lockpicks',
+	weapons = {
+		'lockpick',
+	}
+})
