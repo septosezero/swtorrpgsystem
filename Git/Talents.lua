d1b234a61c3cb98af5eea4638933f3848665c81a
septosezero <35184029+septosezero@users.noreply.github.com>
2018-03-06 06:57:59 -0500

    Create Talents.lua

---
+++ Git/Talents.lua
@@ -0,0 +1,479 @@
+DarkRPG.createJobSkills({
+	team = {TEAM_REPUBLICTRP, TEAM_SITHTRP},
+	stats = {
+		health = '200',
+		movement = '-25',
+		jump = '-25',
+		armor = '50',
+		evasion = '5',
+		reflect = '5',
+		salary = '10%',
+		merchant = '-100',
+		prison = '-100',
+		resists = '10',
+		burn = '5',
+		crush = '5',
+		explode = '3',
+		damage = '5',
+		critical = '5',
+		firerate = '7',
+		magazine = '1',
+		ammo = '-1%',
+		
+	},
+	weaponcategroy = {
+		name = "Blaster Rifles",
+		stats = {
+			damage = '5',
+			critical = '2',
+			firerate = '3',
+			magazine = '5',
+			ammo = '3',
+		},
+	},
+})
+
+DarkRPG.createJobSkills({
+	team = {TEAM_REPUBLICARMYMED, TEAM_IMPARMYMED},
+	stats = {
+		health = '250',
+		movement = '-15',
+		jump = '=-5',
+		armor = '75',
+		evasion = '5',
+		reflect = '5',
+		salary = '15',
+		merchant = '-100',
+		prison = '-100',
+		resists = '5',
+		burn = '7',
+		crush = '5',
+		explode = '5',
+		damage = '2',
+		critical = '5',
+		firerate = '-1',
+		magazine = '-1',
+		ammo = '-1',
+	},
+	weaponcategory = {
+		name = "Blaster Pistols",
+		stats = {
+			critical = '2',
+			firerate = '5',
+			magazine = '5',
+			ammo = '10',
+		},
+	},
+})
+
+DarkRPG.createJobSkills({
+	team = {TEAM_REPUBLICARMYHVY, TEAM_IMPARMYHVY},
+	stats = {
+		health = '400',
+		movement = '-50',
+		jump = '-50',
+		armor = '150',
+		evasion = '5',
+		reflect = '10',
+		salary = '25',
+		merchant = '-100',
+		prison = '-100',
+		resists = '15',
+		burn = '2',
+		explode = '5',
+		damage = '5',
+		critical = '15',
+		firerate = '10',
+		magazine = '10',
+		ammo = '15',
+	},
+	weaponcategory = {
+		name = "Heavy Weapons",
+		stats = {	
+			damage = '5',
+			critical = '2',
+			firerate = '2',
+			ammo = '10',
+		},
+	},
+})
+
+DarkRPG.createJobSkills({
+	team = {TEAM_REPUBLICARMYDEMO, SITHARMYDEMO},
+	stats = {
+		health = '300',
+		movement = '-10',
+		jump = '-10',
+		armor = '100',
+		evasion = '5',
+		reflect = '2',
+		salary = '20',
+		merchant = '-100',
+		prison = '-100',
+		resists = '5',
+		burn = '15',
+		explode = '15',
+		damage = '5',
+		critical = '5',
+		magazine = '5',
+		ammo = '-1',
+	},
+	weaponcategory = {
+		name = "Explosives",
+		stats = {
+			damage = '10',
+			critical = '10',
+			ammo = '5',
+		},
+	},
+})
+
+DarkRPG.createJobSkills({
+	team = {TEAM_REPUBLICARMYSNIP, TEAM_IMPARMYSNIP},
+	stats = {
+		health = '200',
+		movement = '5',
+		jump = '5',
+		armor = '75',
+		evasion = '10',
+		reflect = '10',
+		salary = '20',
+		merchant = '-100',
+		prison = '-100',
+		resists = '5',
+		burn = '5',
+		damage = '5',
+		critical = '5',
+		firerate = '2',
+		magazine = '4',
+		ammo = '-3',
+	},
+	weaponcategory = {
+		name = "Sniper Rifles",
+		stats = {
+			damage = '5',
+			critical = '2',
+			firerate = '1',
+			magazine = '1',
+			ammo = '-2',
+		},
+	},
+})
+
+DarkRPG.createJobSkills({
+	team = {TEAM_REPUBLICARMYSGT, TEAM_SITHARMYSGT},
+	stats = {
+		health = '350',
+		movement = '-10',
+		jump = '-10',
+		armor = '200',
+		evasion = '10',
+		reflect = '5',
+		salary = '25',
+		merchant = '-100',
+		prison = '-100',
+		resists = '5',
+		burn = '5',
+		crush = '2',
+		explode = '2',
+		damage = '3',
+		critical = '3',
+		firerate = '2',
+		magazine = '2',
+		ammo = '2',
+	},
+	weaponcategory = {
+		name = "Blaster Rifles",
+		stats = {
+			damage = '2',
+			critical = '5',
+			firerate = '2',
+			magazine = '3',
+			ammo = '-1',
+		},
+	},
+})
+
+DarkRPG.createJobSkills({
+	team = {TEAM_REPUBLICARMYOFF, TEAM_IMPARMYOFF},
+	stats = {
+		health = '400',
+		movement = '-5',
+		jump = '-5',
+		armor = '200',
+		evasion = '10',
+		reflect = '7',
+		salary = '25',
+		merchant = '-75',
+		prison = '-75',
+		resists = '7',
+		burn = '5',
+		crush = '5',
+		explode = '5',
+		damage = '10',
+		critical = '5',
+		firerate = '5',
+		magazine = '5',
+		ammo = '15',
+	},
+	weaponcategory = {
+		name = "Blaster Pistols",
+		stats = {
+			damage = '5',
+			critical = '5',
+			magazine = '2',
+			ammo = '5',
+		},
+	},
+})
+
+DarkRPG.createJobSkills({
+	team = {TEAM_REPUBLICARMYGEN, TEAM_IMPARMYGEN},
+	stats = {
+		health = '500',
+		movement = '1',
+		jump = '5',
+		armor = '200',
+		evasion = '10',
+		reflect = '12',
+		salary = '25',
+		merchant = '-50',
+		prison = '-50',
+		resists = '10',
+		burn = '5',
+		crush = '5',
+		explode = '5',
+		damage = '15',
+		critical = '10',
+		firerate = '10',
+		magazine = '10',
+		ammo = '15',
+	},
+	weaponcategory = {
+		name = "Blaster Pistols",
+		stats = {
+			damage = '5',
+			critical = '5',
+			magazine = '5',
+			ammo = '10',
+		},
+	},
+})
+
+DarkRPG.createJobSkills({
+	team = {TEAM_JEDIPADAWAN, TEAM_SITHACOLYTE},
+	stats = {
+		health = '150',
+		movement = '-5',
+		jump = '5',
+		armor = '100',
+		evasion = '5',
+		reflect = '5',
+		salary = '10',
+		merchant = '-50',
+		prison = '-100',
+		resists = '5',
+		burn = '5',
+		crush = '10',
+		explode = '5',
+		damage = '5',
+		critical = '5',
+		firerate = '2',
+	},
+	weaponcategory = {
+		name = "Lightsabers",
+		stats = {
+			damage = '1',
+			critical = '3',
+			firerate = '2',
+		},
+	},
+})
+
+DarkRPG.createJobSkills({
+	team = {TEAM_KNIGHTCONSULAR, TEAM_SITHINQUISITOR},
+	stats = {
+		health = '250',
+		movement = '10',
+		jump = '10',
+		armor = '150',
+		evasion = '7',
+		reflect = '7',
+		salary = '15',
+		merchant = '-50',
+		prison = '-100',
+		resists = '10',
+		burn = '10',
+		crush = '5',
+		explode = '5',
+		damage = '5',
+		critical = '7',
+		firerate = '5',
+	},
+	weaponcategory = {
+		name = "Lightsabers",
+		stats = {
+			damage = '3',
+			critical = '4',
+			firerate = '2',
+		},
+	},
+})
+
+DarkRPG.createJobSkills({
+	team = {TEAM_KNIGHTGUARDIAN, TEAM_SITHWARRIOR},
+	stats = {
+		health = '400',
+		movement = '5',
+		jump = '5',
+		armor = '200',
+		evasion = '10',
+		reflect = '5',
+		salary = '15',
+		merchant = '-50',
+		prison = '-100',
+		resists = '5',
+		crush = '5',
+		explode = '5',
+		damage = '5',
+		critical = '10',
+		firerate = '5',
+	},
+	weaponcategory = {
+		name = "Lightsabers",
+		stats = {
+			damage = '5',
+			critical = '7',
+			firerate = '3',
+		},
+	},
+})
+
+DarkRPG.createJobSkills({
+	team = {TEAM_KNIGHTSENTINEL, TEAM_STIHASSASSIN},
+	stats = {
+		health = '300',
+		movement = '10',
+		jump = '7',
+		armor = '200',
+		evasion = '7',
+		reflect = '5',
+		salary = '15',
+		merchant = '-50',
+		prison = '-100',
+		resists = '5',
+		crush = '3',
+		explode = '3',
+		damage = '12',
+		critical = '10',
+		firerate = '10',
+	},
+	weaponcategory = {
+		name = "Lightsabers",
+		stats = {
+			damage = '3',
+			critical = '2',
+		},
+	},
+})
+
+DarkRPG.createJobSkills({
+	team = {TEAM_JEDIMASTER, TEAM_SITHLORD},
+	stats - {
+		health = '650',
+		movement = '15',
+		jump = '15',
+		armor = '200',
+		evasion = '12',
+		reflect = '15',
+		salary = '10',
+		merchant = '-25',
+		prison = '-100',
+		resists = '10',
+		crush = '7',
+		burn = '10',
+		explode = '5',
+		damage = '10',
+		critical = '15',
+		firerate = '10',
+	},
+	weaponcategory = {
+		name = "Lightsabers",
+		stats = {
+			damage = '5',
+			critical = '2',
+			firerate = '2',
+		},
+	},
+})
+
+DarkRPG.createJobSkills({
+	team = {TEAM_CITIZEN, TEAM_SMUGGLER},
+	stats = {
+		health = '200',
+		movement = '2',
+		jump = '2',
+		armor = '50',
+		evasion = '3',
+		reflect = '2',
+		salary = '25',
+		prison = '-5',
+		resists = '5',
+		burn = '5',
+		crush = '2',
+		explode = '1',
+		damage = '2',
+		critical = '2',
+		firerate = '2',
+		magazine = '2',
+		ammo = '10',
+	},
+	weaponcategory = {
+		name = "Blaster Pistols",
+		stats = {
+			damage = '5',
+			critical = '3',
+			magazine = '5',
+			ammo = '5',
+		},
+	},
+})
+
+DarkRPG.createJobSkills({
+	team = {TEAM_DOCTOR},
+	stats = {
+		health = '250',
+		movement = '10',
+		jump = '10',
+		armor = '50',
+		evasion = '10',
+		reflect = '2',
+		salary = '10',
+		prison = '-5',
+		resists = '3',
+		burn = '1',
+		crush = '2',
+		damage = '-10',
+		critical = '10',
+		firerate = '10',
+		magazine = '7',
+		ammo = '15',
+	},
+	weaponcategory = {
+		name = "Blaster Rifles",
+		stats = {
+			damage = '10',
+			critical = '3',
+			firerate = '-4',
+			magazine = '-4',
+			ammo = '-10',
+		},
+	},
+})
+
+--   						TALENT TREES (^^CREATE NAVY SKILLS)
+
+DarkRPG.createTalent({
+	name = "
+		
+		
