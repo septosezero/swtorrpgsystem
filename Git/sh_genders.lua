d29c1b49a285a1cd3d3ec3885a89ed6288df53ab
septosezero <35184029+septosezero@users.noreply.github.com>
2018-03-11 22:45:47 -0400

    Create sh_genders.lua

---
+++ Git/sh_genders.lua
@@ -0,0 +1,11 @@
+local gender = {}
+gender.Name = "Male"
+gender.Icon = "sci_char/gender/male_active.png"
+
+SChar.Action:loadGenderTbl(gender)
+
+local gender = {}
+gender.Name = "Female"
+gender.Icon = "sci_char/gender/female_active.png"
+
+SChar.Action:loadGenderTbl(gender)
