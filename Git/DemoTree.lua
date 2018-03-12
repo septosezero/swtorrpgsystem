f27e0e46fafbb3ca52369848793dfc65bcd4b289
septosezero <35184029+septosezero@users.noreply.github.com>
2018-03-12 05:49:30 -0400

    Create DemoTree.lua

---
+++ Git/DemoTree.lua
@@ -0,0 +1,156 @@
+--[[
+███████████████████████████████████████████████████████████████████████████████████████████████████████
+█░░░░░░░░░░░░░░███░░░░░░░░░░░░░░█░░░░░░██████████░░░░░░█░░░░░░░░░░░░███░░░░░░░░░░█░░░░░░░░░░░░░░███████
+█░░▄▀▄▀▄▀▄▀▄▀░░███░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░░░░░░░░░██░░▄▀░░█░░▄▀▄▀▄▀▄▀░░░░█░░▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░███████
+█░░▄▀░░░░░░▄▀░░███░░▄▀░░░░░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░██░░▄▀░░█░░▄▀░░░░▄▀▄▀░░█░░░░▄▀░░░░█░░░░░░▄▀░░░░░░███████
+█░░▄▀░░██░░▄▀░░███░░▄▀░░██░░▄▀░░█░░▄▀░░░░░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀░░███░░▄▀░░███████░░▄▀░░███████████
+█░░▄▀░░░░░░▄▀░░░░█░░▄▀░░░░░░▄▀░░█░░▄▀░░██░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀░░███░░▄▀░░███████░░▄▀░░███████████
+█░░▄▀▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░██░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀░░███░░▄▀░░███████░░▄▀░░███████████
+█░░▄▀░░░░░░░░▄▀░░█░░▄▀░░░░░░▄▀░░█░░▄▀░░██░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀░░███░░▄▀░░███████░░▄▀░░███████████
+█░░▄▀░░████░░▄▀░░█░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀░░░░░░▄▀░░█░░▄▀░░██░░▄▀░░███░░▄▀░░███████░░▄▀░░███████████
+█░░▄▀░░░░░░░░▄▀░░█░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░░░▄▀▄▀░░█░░░░▄▀░░░░█████░░▄▀░░███████████
+█░░▄▀▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░██░░▄▀░░█░░▄▀░░██░░░░░░░░░░▄▀░░█░░▄▀▄▀▄▀▄▀░░░░█░░▄▀▄▀▄▀░░█████░░▄▀░░███████████
+█░░░░░░░░░░░░░░░░█░░░░░░██░░░░░░█░░░░░░██████████░░░░░░█░░░░░░░░░░░░███░░░░░░░░░░█████░░░░░░███████████
+███████████████████████████████████████████████████████████████████████████████████████████████████████
+███████████████████████████████████████████████████████████████████████████████████████████████████████
+█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░██████████░░░░░░█░░░░░░░░░░█░░░░░░██████████░░░░░░█░░░░░░░░░░░░░░█
+█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░░░░░░░░░░░░░▄▀░░█░░▄▀▄▀▄▀░░█░░▄▀░░░░░░░░░░██░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█
+█░░▄▀░░░░░░░░░░█░░▄▀░░░░░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀░░█░░░░▄▀░░░░█░░▄▀▄▀▄▀▄▀▄▀░░██░░▄▀░░█░░▄▀░░░░░░░░░░█
+█░░▄▀░░█████████░░▄▀░░██░░▄▀░░█░░▄▀░░░░░░▄▀░░░░░░▄▀░░███░░▄▀░░███░░▄▀░░░░░░▄▀░░██░░▄▀░░█░░▄▀░░█████████
+█░░▄▀░░█████████░░▄▀░░░░░░▄▀░░█░░▄▀░░██░░▄▀░░██░░▄▀░░███░░▄▀░░███░░▄▀░░██░░▄▀░░██░░▄▀░░█░░▄▀░░█████████
+█░░▄▀░░██░░░░░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░██░░▄▀░░██░░▄▀░░███░░▄▀░░███░░▄▀░░██░░▄▀░░██░░▄▀░░█░░▄▀░░██░░░░░░█
+█░░▄▀░░██░░▄▀░░█░░▄▀░░░░░░▄▀░░█░░▄▀░░██░░░░░░██░░▄▀░░███░░▄▀░░███░░▄▀░░██░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀░░█
+█░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀░░█░░▄▀░░██████████░░▄▀░░███░░▄▀░░███░░▄▀░░██░░▄▀░░░░░░▄▀░░█░░▄▀░░██░░▄▀░░█
+█░░▄▀░░░░░░▄▀░░█░░▄▀░░██░░▄▀░░█░░▄▀░░██████████░░▄▀░░█░░░░▄▀░░░░█░░▄▀░░██░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░░░░░▄▀░░█
+█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░██░░▄▀░░█░░▄▀░░██████████░░▄▀░░█░░▄▀▄▀▄▀░░█░░▄▀░░██░░░░░░░░░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█
+█░░░░░░░░░░░░░░█░░░░░░██░░░░░░█░░░░░░██████████░░░░░░█░░░░░░░░░░█░░░░░░██████████░░░░░░█░░░░░░░░░░░░░░█
+███████████████████████████████████████████████████████████████████████████████████████████████████████
+				--[[Server IP: 	74.91.124.2]]--
+				--[[Made by: SeptoseZero   ]]--
+				--[[    Demolitions Tree   ]]--
+--]]
+
+DarkRPG.createJobSkills({
+	team = {TEAM_REPUBLICARMYDEMO, SITHARMYDEMO},
+	stats = {
+		health = '300',
+		movement = '-10',
+		jump = '-10',
+		armor = '100',
+		evasion = '5',
+		reflect = '2',
+		salary = '20',
+		merchant = '-100',
+		prison = '-100',
+		resists = '5',
+		burn = '15',
+		explode = '15',
+		damage = '5',
+		critical = '5',
+		magazine = '5',
+		ammo = '-1',
+	},
+	weaponcategory = {
+		name = "Explosives",
+		stats = {
+			damage = '10',
+			critical = '10',
+			ammo = '5',
+		},
+	},
+})
+
+DarkRPG.createTalent({
+	name = "Launchers Proficiency",
+	team = {TEAM_REPUBLICARMYDEMO, TEAM_IMPARMYDEMO},
+	pos = {1,1},
+	thumb = "BlasterRifles.png",
+	ranks = 5,
+	give = "tfa_swch_clonelauncher",
+	desc = "Training in Launchers will allow you to carry one at all times, and will increase your abilities with them.",
+	weaponcategory = {
+		name = "Launchers",
+		stats = {
+			damage = '5',
+			critical = '3',
+			ammo = '5',
+		},
+	},
+	weaponcategory = {
+		name = "Blaster Pistols",
+		stats = {
+			damage = '-2',
+			critical = '-2',
+		},
+	},
+})
+
+DarkRPG.createTalent({
+	name = "Blaster Pistol Proficiency",
+	team = {TEAM_REPUBLICARMYDEMO, TEAM_IMPARMYDEMO},
+	pos = {2,1},
+	thumb = "BlasterPistols.png",
+	ranks = 5,
+	give = "tfa_kotor_bp_2",
+	desc = "Training in Blaster Pistols will allow you to carry one at all times, and will increase your abilities with them.",
+	weaponcategory = {
+		name = "Blaster Pistols",
+		stats = {
+			damage = '5',
+			critical = '3',
+		},
+	},
+	weaponcategory = {
+		name = "Launchers",
+		stats = {
+			damage = '-2',
+			critical = '-2',
+		},
+	},
+})
+
+DarkRPG.createTalent({
+	name = "Blaster Pistol Mastery",
+	team = {TEAM_REPUBLICARMYDEMO, TEAM_IMPARMYDEMO},
+	pos = {1,3},
+	ranks = 3,
+	thumb = "BlasterPistols2.png",
+	desc = "Blaster Pistol Mastery will increase your abilities with Blaster Pistols even further.",
+	weaponcategory = {
+		name = "Blaster Pistols",
+		stats = {
+			damage = '5',
+			critical = '3',
+		},
+	},
+	weaponcategory = {
+		name = "Launchers",
+		stats = {
+			damage = '-5',
+			critical = '-3',
+		},
+	},
+})
+
+DarkRPG.createTalent({
+	name = "Launchers Mastery",
+	team = {TEAM_REPUBLICARMYDEMO, TEAM_IMPARMYDEMO},
+	pos = {2,3},
+	ranks = 3,
+	thumb = "BlasterRifles2.png",
+	desc = "Launchers Mastery will increase your abilities with Launchers even further.",
+	weaponcategory = {
+		name = "Launchers",
+		stats = {
+			damage = '5',
+			critical = '3',
+		},
+	},
+	weaponcategory = {
+		name = "Blaster Pistols",
+		stats = {
+			damage = '-5',
+			critical = '-3',
+		},
+	},
+})
