46565b259e2f4ff355f7764f7d865e0ec2990e70
septosezero <35184029+septosezero@users.noreply.github.com>
2018-03-14 05:33:19 -0400

    Update addjobs.lua
    replace current Force User jobs with these, for testing new talents

--- Git/addjobs.lua
+++ Git/addjobs.lua
@@ -43,3 +43,63 @@ TEAM_SITHAPP = DarkRP.createJob("Sith Apprentice", {
     category = "Sith Order",
  })
 
+TEAM_SITHLORD = DarkRP.createJob("Sith Lord", {
+	color = Color(240, 7, 7, 255),
+        model = {"models/player/sith/human.mdl", "models/player/sith/bith.mdl", "models/player/sith/gotal.mdl", "models/player/sith/gungan.mdl", "models/player/sith/nautolan.mdl", "models/player/sith/pantoran.mdl", "models/player/sith/quarren.mdl", "models/player/sith/rodian.mdl", "models/player/sith/togruta.mdl", "models/player/sith/trandoshan.mdl", "models/player/sith/twilek.mdl", "models/player/sith/twilek2.mdl", "models/player/sith/umbaran.mdl", "models/player/sith/zabrak.mdl"},
+   	description = [[A Sith Lord, responsible for training Acolytes and Apprentices.]],
+	weapons = {"keys", "climb_swep2"},
+	command = "SITHLORD",
+	max = 5,
+	salary = 750,
+	admin = 0,
+	vote = false,
+	hasLicense = true,
+	candemote = false,
+	category = "Sith Order",
+})
+
+TEAM_SITHDARKLORD = DarkRP.createJob("Dark Lord of the Sith", {
+	color = Color(240, 7, 7, 255),
+	model = "" -- Valkorian MDL from Eternal Pack
+	description = [[The Dark Lord of the Sith Order, leader of the Sith Empire.]],
+	weapons = {"keys", "climb_swep2"},
+	command = "SITHDARKLORD",
+	max = 1,
+	salary = 1000,
+	admin = 0,
+	vote = false,
+	hasLicense = true,
+	candemote = false,
+	category = "Sith Order",
+})
+
+TEAM_JEDIKNIGHT = DarkRP.createJob("Jedi Knight", {
+	color = Color(222, 166, 6, 255),
+	model = {"models/player/jedi/human.mdl", "models/player/jedi/bith.mdl", "models/player/jedi/gotal.mdl", "models/player/jedi/gungan.mdl", "models/player/jedi/nautolan.mdl", "models/player/jedi/pantoran.mdl", "models/player/jedi/quarren.mdl", "models/player/jedi/rodian.mdl", "models/player/jedi/togruta.mdl", "models/player/jedi/trandoshan.mdl", "models/player/jedi/twilek.mdl", "models/player/jedi/twilek2.mdl", "models/player/jedi/umbaran.mdl", "models/player/jedi/zabrak.mdl"},
+   	description = [[A Jedi Knight, tasked with training a Padawan.]],
+	weapons = {"keys", "climb_swep2"},
+	command = "JEDIKNIGHT",
+	max = 5,
+	salary = 750,
+	admin = 0,
+	vote = false,
+	hasLicense = true,
+	candemote = false,
+	category = "Jedi Order",
+})
+
+TEAM_JEDIMASTER = DarkRP.createJob("Jedi Master", {
+	color = Color(222, 166, 6, 255),
+	model = {"models/player/jedi/human.mdl", "models/player/jedi/bith.mdl", "models/player/jedi/gotal.mdl", "models/player/jedi/gungan.mdl", "models/player/jedi/nautolan.mdl", "models/player/jedi/pantoran.mdl", "models/player/jedi/quarren.mdl", "models/player/jedi/rodian.mdl", "models/player/jedi/togruta.mdl", "models/player/jedi/trandoshan.mdl", "models/player/jedi/twilek.mdl", "models/player/jedi/twilek2.mdl", "models/player/jedi/umbaran.mdl", "models/player/jedi/zabrak.mdl"},
+	weapons = {"keys", "climb_swep2"},
+	command = "JEDIMASTER",
+	max = 5,
+	salary = 1000,
+	admin = 0,
+	vote = false,
+	hasLicense = true,
+	candemote = false,
+	category = "Jedi Order",
+})
+
+
